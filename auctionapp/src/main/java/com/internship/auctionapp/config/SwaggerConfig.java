60b6901fbff31f8fcebfed306e7bf32e5a7463e1
semrakermo <85747236+skermo@users.noreply.github.com>
2023-03-10 15:16:53 +0100

    Update app skeleton

--- auctionapp/src/main/java/com/internship/auctionapp/config/SwaggerConfig.java
+++ auctionapp/src/main/java/com/internship/auctionapp/config/SwaggerConfig.java
@@ -4,14 +4,15 @@ package com.internship.auctionapp.config;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 
-public class SwaggerConfig {
+/**
+ * Make Swagger documentation available at swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config
+ */
 
-    //http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config
+public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi publicApi(){
         return GroupedOpenApi.builder()
-                .group("sve")
                 .pathsToMatch("/**")
                 .build();
     }
