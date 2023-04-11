70fec2e186d329522dfa8b116deb0801c98ef8b9
semrakermo <85747236+skermo@users.noreply.github.com>
2023-04-11 15:02:10 +0200

    Implement login, registration and logout functionalities

---
+++ auctionapp/src/main/java/com/internship/auctionapp/security/jwt/JwtEntryPoint.java
@@ -0,0 +1,29 @@
+package com.internship.auctionapp.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        final ObjectMapper mapper = new ObjectMapper();
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        mapper.writeValue(response.getOutputStream(), authException.getMessage());
+    }
+}
