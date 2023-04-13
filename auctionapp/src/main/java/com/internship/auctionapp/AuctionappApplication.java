47e0075cb2bdf054ca0d0584ac04158516029ebb
semrakermo <85747236+skermo@users.noreply.github.com>
2023-04-13 11:19:43 +0200

    Uncommit .idea files

--- auctionapp/src/main/java/com/internship/auctionapp/AuctionappApplication.java
+++ auctionapp/src/main/java/com/internship/auctionapp/AuctionappApplication.java
@@ -1,20 +1,11 @@
 package com.internship.auctionapp;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Auction App",
-				version = "1.0.0",
-				description = "Online marketplace for selling, bidding on & buying products."
-		)
-)
 public class AuctionappApplication {
 	@Bean
 	public ModelMapper modelMapper(){
